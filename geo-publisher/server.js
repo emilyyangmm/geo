f805faae9ecf2d6e933770fe0ba63bc89fab3e98
emilyyangmm <239927937@qq.com>
2026-07-16 14:40:25 +0800

    Fix GEO login CORS and signup redirect

--- geo-publisher/server.js
+++ geo-publisher/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-GEO-User-Id');
   res.header('Access-Control-Allow-Private-Network', 'true');
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
